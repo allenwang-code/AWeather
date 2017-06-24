8fd485a0c9aabc4f8e19dd553dd201a41862b286
Allen Wang <allenwang.appdeveloper@gmail.com>
2017-06-24 21:32:01 +0800

    add launch image and app icon

---
+++ Weather/WelcomPageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewController.swift
+//  Weather
+//
+//  Created by Allen Wang on 2017/6/24.
+//  Copyright © 2017年 Allen Wang. All rights reserved.
+//
+
+import UIKit
+
+class WelcomPageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
