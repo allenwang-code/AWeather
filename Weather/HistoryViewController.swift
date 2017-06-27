e649f4875f8355157143c12fb21ae09cebd6f494
Allen Wang <s972024@gmail.com>
2017-06-28 00:51:02 +0800

    start to do history list and hourly list

---
+++ Weather/HistoryViewController.swift
@@ -0,0 +1,48 @@
+//
+//  HistoryViewController.swift
+//  Weather
+//
+//  Created by Allen Wang on 2017/6/28.
+//  Copyright © 2017年 Allen Wang. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(
+                withIdentifier: "Cell", for: indexPath) as
+        UITableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+}
